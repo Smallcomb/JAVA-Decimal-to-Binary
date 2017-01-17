5ccdcb9b107c3ecdc6912a396b7b2faeaeb7354e
Smallcomb <AlexSmallcomb@gmail.com>
2017-01-17 15:50:33 -0500

    Final

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -1,40 +1,58 @@
 // Binary Converter - Develop a converter to convert a decimal 
 // number to binary or a binary number to its decimal equivalent.
 
-import java.lang.Math.*;
+import java.util.Scanner;
 
 public class BinaryConverter {
 	
-	// find greatest 2^n value thats < given userDec
-	double findTopDigit (double userDec)
-	{
+	
+	//static double findTopDigit (double userDec)
+	//{
 		
+
+
+			//return topDigit;
+	//}
+	
+	static String decToBinary (double userDec)
+	{	
+		String bin = "";
+		
+		// find greatest 2^i value thats <= input (leftmost binary)
+		double topDigit = 0;
 		for(int i=1;Math.pow(2,i)<=userDec;i++)
 		{
-			double topDigit = i;		
+			topDigit++;
 		}
-		return topDigit;
-	}
-	
-	String decToBinary (int topDigit, double userDec)
-	{	
-		for(int j = topDigit; j>=1; j--)
+		
+		// loop through binary digit spaces, starting with greatest/leftmost
+		for(double j = topDigit; j>=0; j--)
 		{
+
 			// if userDec >= 2^digit, subtract top digit from x, append 1 to string
 			if(userDec >= Math.pow(2,j))
 			{
-				userDec = userDec - Math.pow(2,j);
+				double square = Math.pow(2,j);
+				userDec = (userDec - square);
+				bin = bin.concat("1");
 			}
+			
 			// if userDec < 2^digit, append 0 to string
-		}
-		
-		String y;
-		return y;
+			else
+			{
+				bin = bin.concat("0");
+			}
+		}		
+		return bin;
 	}
 	
 	public static void main(String[] args) 
 	{
-
+		System.out.println("Enter Integer");
+		
+		Scanner scanner = new Scanner(System.in);
+		double userDec = Integer.parseInt(scanner.nextLine());
+		
+		System.out.println(decToBinary(userDec));
 	}
-
 }
